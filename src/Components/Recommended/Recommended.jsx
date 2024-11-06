8623428a64571c9940202bb99842e51f6614afdb
Hasanka123-hdz <hasankadeshapriya97@gmail.com>
2024-11-06 18:04:24 +0530

    feat: recommended video play

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -9,13 +9,14 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png' 
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { API_KEY } from '../../data'
+import { Link } from 'react-router-dom'
 
 const Recommended = ({categoryId}) => {
   const [apiData,setApiData] = useState([]);
   console.log(categoryId);
 
   const fetchData = async () => {
-    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
     await fetch(relatedVideo_url).then(res=>res.json()).then(data=>setApiData(data.items))        
   }
 
@@ -27,13 +28,14 @@ const Recommended = ({categoryId}) => {
     <div className='recommended'>
       {apiData.map((item,index)=>{
         return(
-        <div key={index} className="side-video-list"> 
-            <img src={thumbnail1} alt="" />
+        <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className="side-video-list">
+          <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className="vid-info">
-                <h4>Good channel that help you to be a web developer</h4> <p>WebDotDev</p>
-                <p>199K Vigews</p>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{item.statistics.viewCount} Views</p>
             </div>
-        </div>
+        </Link>
         );
       })}
     </div>
